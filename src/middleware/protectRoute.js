a95123d4fe85477b8b3b50102d8eb1e6416d4b00
Aryan Singh <97840303+OaryanO@users.noreply.github.com>
2026-02-17 22:03:23 +0530

    test protectroute

--- src/middleware/protectRoute.js
+++ src/middleware/protectRoute.js
@@ -1,3 +1,31 @@
+// import { requireAuth } from "@clerk/express";
+// import User from "../models/User.js";
+
+// export const protectRoute = [
+//   requireAuth(),
+//   async (req, res, next) => {
+//     try {
+//       const clerkId = req.auth.userId;
+
+//       if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
+
+//       // find user in db by clerk ID
+//       const user = await User.findOne({ clerkId });
+
+//       if (!user) return res.status(404).json({ message: "User not found" });
+
+//       // attach user to req
+//       req.user = user;
+
+//       next();
+//     } catch (error) {
+//       console.error("Error in protectRoute middleware", error);
+//       res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   },
+// ];
+
+
 import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
@@ -7,14 +35,18 @@ export const protectRoute = [
     try {
       const clerkId = req.auth.userId;
 
-      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
+      if (!clerkId) {
+        return res.status(401).json({ message: "Unauthorized - invalid token" });
+      }
 
-      // find user in db by clerk ID
-      const user = await User.findOne({ clerkId });
+      let user = await User.findOne({ clerkId });
 
-      if (!user) return res.status(404).json({ message: "User not found" });
+      if (!user) {
+        user = await User.create({
+          clerkId,
+        });
+      }
 
-      // attach user to req
       req.user = user;
 
       next();
@@ -24,3 +56,4 @@ export const protectRoute = [
     }
   },
 ];
+
